8ab4b6eca1c79f5d6cf91e852acf36c60ebdf699
Abhijeet Singh <abhijeetsinghkgp@gmail.com>
2017-03-04 20:59:32 +0530

    Latest starter code + dialog fragment changes

---
+++ app/src/main/java/com/udacity/stockhawk/ui/AddStockDialog.java
@@ -0,0 +1,106 @@
+package com.udacity.stockhawk.ui;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.udacity.stockhawk.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+public class AddStockDialog extends DialogFragment {
+
+    @SuppressWarnings("WeakerAccess")
+    @BindView(R.id.dialog_stock)
+    EditText stock;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = LayoutInflater.from(getActivity());
+        @SuppressLint("InflateParams") View custom = inflater.inflate(R.layout.add_stock_dialog, null);
+
+        ButterKnife.bind(this, custom);
+
+        stock.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                addStock();
+                return true;
+            }
+        });
+        builder.setView(custom);
+
+        builder.setMessage(getString(R.string.dialog_title));
+        builder.setPositiveButton(getString(R.string.dialog_add),
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        addStock();
+                    }
+                });
+        builder.setNegativeButton(getString(R.string.dialog_cancel), null);
+
+        final Dialog dialog = builder.create();
+
+        EditText stockEditText = (EditText) custom.findViewById(R.id.dialog_stock);
+        stockEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // Check if edittext is empty
+                if (TextUtils.isEmpty(s)) {
+                    // Disable ok button
+                    ((AlertDialog) dialog).getButton(
+                            AlertDialog.BUTTON_POSITIVE).setEnabled(false);
+                } else {
+                    // Something into edit text. Enable the button.
+                    ((AlertDialog) dialog).getButton(
+                            AlertDialog.BUTTON_POSITIVE).setEnabled(true);
+                }
+            }
+        });
+        Window window = dialog.getWindow();
+        if (window != null) {
+            window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+        }
+
+        return dialog;
+    }
+
+    private void addStock() {
+        Activity parent = getActivity();
+        if (parent instanceof MainActivity) {
+            ((MainActivity) parent).addStock(stock.getText().toString().toUpperCase());
+        }
+        dismissAllowingStateLoss();
+    }
+
+
+}
